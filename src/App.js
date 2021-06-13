2335dd6f65a62816e23cfd3d5ba0c0bb66187bb2
Julien.Sacher <74544925+JulienSacher@users.noreply.github.com>
2021-06-13 22:07:40 +0200

    Data file deleted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import FirstSection from "./components/FirstSection";
