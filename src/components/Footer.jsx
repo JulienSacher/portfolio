3e1f0f586bff7556d4ce9fc28fcd8987775e2bb0
Julien.Sacher <74544925+JulienSacher@users.noreply.github.com>
2021-06-15 17:14:31 +0200

    Theme colors updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
     <div className="footer__container">
       <Container>
         <Col className="d-flex justify-content-center pt-5 pb-2">
-          <a href="https://www.linkedin.com/in/julien-sacher/" target="_blank">
+          <a href="https://www.linkedin.com/in/julien-sacher/" target="_blank" rel="noreferrer">
             <LinkedInIcon className="mx-2" />
           </a>
-          <a href="https://github.com/JulienSacher" target="_blank">
+          <a href="https://github.com/JulienSacher" target="_blank"rel="noreferrer">
             <GitHubIcon className="mx-2" />
           </a>
         </Col>
